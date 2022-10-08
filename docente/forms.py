8ad1597221edcb44114b9236950bf28a31757eb3
munoz437 <jorgeelias437@gmail.com>
2022-10-07 22:54:44 -0600

    fecha docente

--- docente/forms.py
+++ docente/forms.py
@@ -1,7 +1,11 @@
 from django import forms
 from .models import Docente
 
+class DateInput(forms.DateInput):
+    input_type='date'
+
 class DocenteForm(forms.ModelForm):
     class Meta:
         model = Docente
         fields ='__all__'
+        widgets = {'fecha_nacimiento': DateInput(), }
